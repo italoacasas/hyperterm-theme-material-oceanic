05176ee20c250bc7be16518eec44f8991cc6647e
Italo A. Casas <me@italoacasas.com>
2016-07-17 17:14:14 -0400

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# HyperTerm Theme Oceanic Material
+
+## Contains
+
+- [Install](#install)
+- [License](#license)
+
+## Install
+
+Add `hyperterm-theme-material-oceanic` to your plugins list on `~/.hyperterm.js`
+
+## License
+
+See [LICENSE.md](./LICENSE.md)
---
+++ index.js
@@ -0,0 +1,40 @@
+exports.decorateConfig = (config) => {
+  return Object.assign({}, config, {
+    backgroundColor: '#1B2B34',
+    borderColor: '#1B2B34',
+    cursorColor: 'rgba(0, 150, 136, .5)',
+    colors: [
+      '#1B2B34',
+      '#EC5f67',
+      '#4F5B66',
+      '#65737E',
+      '#A7ADBA',
+      '#C0C5CE',
+      '#CDD3DE',
+      '#D8DEE9',
+      '#212a32',
+      '#F99157',
+      '#FAC863',
+      '#99C794',
+      '#5FB3B3',
+      '#6699CC',
+      '#5FB3B3',
+      '#AB7967'
+    ],
+    css: `
+      ${config.css || ''}
+      .hyperterm_main {
+        border: none !important;
+      }
+      .header_header {
+        background: #212a32 !important;
+      }
+      .tab_tab {
+        border: 0;
+      }
+      .tab_active::before {
+        border-bottom: 2px solid #5FB3B3;
+      }
+    `
+  })
+}
